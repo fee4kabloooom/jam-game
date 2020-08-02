0feb77aa11bb9f0602dddfd7731c2f1c8bed507d
MetrosRU <61288795+MetrosRU@users.noreply.github.com>
2020-08-02 16:15:44 +0400

    Ходьба врагов за счёт colliders
    Стрельбу осталось сделать

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,7 +8,7 @@ public class EnemyController : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject shootPoint;
     public NavMeshAgent agent;
-    Vector2 target;
+    GameObject target;
     public float lookRadius = 5f;
 
     private void Start()
@@ -20,24 +20,38 @@ public class EnemyController : MonoBehaviour
     }
     private void Update()
     {
-        target = GameController.Instance().hero.transform.position;
-        float distance = Vector2.Distance(target, transform.position);
-        RaycastHit2D hit;
-        if (distance <= lookRadius)
-        {
-            hit = Physics2D.Raycast(transform.position, target, lookRadius);
-            if (hit.collider.gameObject == GameController.Instance().hero)
-                Shoot(target);
-            agent.SetDestination(target);
+        //target = GameController.Instance().hero.transform.position;
+        if (target != null) {
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(transform.position, target.transform.position, lookRadius);
+            if (hit.collider.gameObject == target)
+            {
+                print("ПИФ-ПАФ");
+                Shoot(target.transform.position);
+            }
         }
-    }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position,lookRadius);
     }
     private void Shoot(Vector2 destination) {
         GameObject b = GameObject.Instantiate(bullet, shootPoint.transform);
-        b.GetComponent<Rigidbody2D>().AddForce(destination * 10f, ForceMode2D.Impulse);
+        b.GetComponent<Rigidbody2D>().AddRelativeForce(destination * 10f, ForceMode2D.Impulse);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        print("А Я ВСЁ ДУМАЛ, КОГДА ЖЕ ТЫ ПОЯВИШЬСЯ");
+        if (collision.gameObject.GetComponent<HP>().race == HP.Race.Hero)
+        {
+            target = collision.gameObject;
+            agent.SetDestination(target.transform.position);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("ТЫ КУДА?!");
+        if (collision.gameObject.GetComponent<HP>().race == HP.Race.Hero)
+        {
+            target = null;
+            Vector2 lastSeen = collision.transform.position;
+            agent.SetDestination(lastSeen);
+        }
     }
 }
